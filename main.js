36968a289e4cc45cb012be9042f7cf80c2c21dbe
hi-ashleyj <ajor023@gmail.com>
2021-01-05 20:57:34 +1300

    Update test game to match api

--- main.js
+++ main.js
@@ -15,14 +15,21 @@ let Ahalfcircle = new Asset("halfcircle", {
     angleTo: 180
 });
 
-let AhalfcircleI = Ahalfcircle.clone("halfcircleI", { arcAngleS: Ahalfcircle.arcAngleE, arcAngleE: Ahalfcircle.arcAngleS, fill: "#7700cc" });
+let AhalfcircleI = Ahalfcircle.clone("halfcircleI", { fill: "#7700cc" });
 
 Lbase.add(Abackground, Ahalfcircle);
 
 Lcontent.add(AhalfcircleI);
 
-Abackground.draw(0, 0, Game.width, Game.height);
+Abackground.position(0, 0, Game.width, Game.height);
 
-Ahalfcircle.draw(...Asset.center(Game.width / 2, Game.height / 2, 300, 300));
+Ahalfcircle.position(...Asset.center(Game.width / 2, Game.height / 2, 300, 300));
 
-AhalfcircleI.draw(...Asset.center(Game.width / 2, Game.height / 2, 200, 200));
+AhalfcircleI.position(...Asset.center(Game.width / 2, Game.height / 2, 200, 200));
+
+let AnimateHalfcircleF = new Animate.property(Ahalfcircle, "angleFrom", 1000, { from: 0, to: 360 }, Infinity);
+let AnimateHalfcircleT = new Animate.property(Ahalfcircle, "angleTo", 1000, { from: 180, to: 540 }, Infinity);
+let AnimateHalfcircleIF = new Animate.property(AhalfcircleI, "angleFrom", 1000, { from: 180, to: 540 }, Infinity);
+let AnimateHalfcircleIT = new Animate.property(AhalfcircleI, "angleTo", 1000, { from: 360, to: 720 }, Infinity);
+
+Game.start();
