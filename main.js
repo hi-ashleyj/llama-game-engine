7194cd585e7a00f0449a1d5b1a20332ea235ee8c
hi-ashleyj <ajor023@gmail.com>
2021-01-05 21:36:36 +1300

    Make game script more interesting

--- main.js
+++ main.js
@@ -17,27 +17,33 @@ let Ahalfcircle = new Asset("halfcircle", {
 
 let AhalfcircleI = Ahalfcircle.clone("halfcircleI", { fill: "#7700cc" });
 
-let AbouncingDot = new Asset("bouncedot", {
+let ABD1 = new Asset("bouncedot1", {
     primitive: "circle",
     fill: "#db188a"
 });
 
+let ABD2 = ABD1.clone("bouncedot2");
+let ABD3 = ABD1.clone("bouncedot3");
+
 Lbase.add(Abackground, Ahalfcircle);
 
-Lcontent.add(AhalfcircleI, AbouncingDot);
+Lcontent.add(AhalfcircleI, ABD1, ABD2, ABD3);
 
 Abackground.position(0, 0, Game.width, Game.height);
 
 Ahalfcircle.position(Game.width / 2, Game.height / 2, 300, 300);
 AhalfcircleI.position(Game.width / 2, Game.height / 2, 200, 200);
 
-AbouncingDot.position(Game.width / 2, Game.height / 2, 30, 30);
+ABD1.position((Game.width / 2) - 50, Game.height / 2, 30, 30);
 
 let AnimateHalfcircleF = new Animate.property(Ahalfcircle, "angleFrom", 1000, { 0: 0, 1: 360 }, Infinity);
 let AnimateHalfcircleT = new Animate.property(Ahalfcircle, "angleTo", 1000, { 0: 180, 1: 540 }, Infinity);
 let AnimateHalfcircleIF = new Animate.property(AhalfcircleI, "angleFrom", 1000, { 0: 180, 1: 540 }, Infinity);
 let AnimateHalfcircleIT = new Animate.property(AhalfcircleI, "angleTo", 1000, { 0: 360, 1: 720 }, Infinity);
 
-let AnimateHalfcircleIX = new Animate.property(AbouncingDot, "x", 3000, { 0: (Game.width / 2) - 50, 0.5: (Game.width / 2) + 50, 1: (Game.width / 2) - 50 }, Infinity);
+let ABD1X = new Animate.property(ABD1, "x", 3000, { 0: (Game.width / 2) - 50, 0.5: (Game.width / 2) + 50, 1: (Game.width / 2) - 50 }, Infinity);
 
 Game.start();
+
+Game.wait(() => { new Animate.property(ABD2, "x", 3000, { 0: (Game.width / 2) - 50, 0.5: (Game.width / 2) + 50, 1: (Game.width / 2) - 50 }, Infinity); }, 200);
+Game.wait(() => { new Animate.property(ABD3, "x", 3000, { 0: (Game.width / 2) - 50, 0.5: (Game.width / 2) + 50, 1: (Game.width / 2) - 50 }, Infinity); }, 400);
