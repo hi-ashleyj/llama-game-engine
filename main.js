4ab26646b5f8349a9b00befbaba9feaa908ea58d
hi-ashleyj <ajor023@gmail.com>
2021-01-05 21:16:44 +1300

    update test game and add new scripts

--- main.js
+++ main.js
@@ -27,9 +27,11 @@ Ahalfcircle.position(...Asset.center(Game.width / 2, Game.height / 2, 300, 300))
 
 AhalfcircleI.position(...Asset.center(Game.width / 2, Game.height / 2, 200, 200));
 
-let AnimateHalfcircleF = new Animate.property(Ahalfcircle, "angleFrom", 1000, { from: 0, to: 360 }, Infinity);
-let AnimateHalfcircleT = new Animate.property(Ahalfcircle, "angleTo", 1000, { from: 180, to: 540 }, Infinity);
-let AnimateHalfcircleIF = new Animate.property(AhalfcircleI, "angleFrom", 1000, { from: 180, to: 540 }, Infinity);
-let AnimateHalfcircleIT = new Animate.property(AhalfcircleI, "angleTo", 1000, { from: 360, to: 720 }, Infinity);
+let AnimateHalfcircleF = new Animate.property(Ahalfcircle, "angleFrom", 1000, { 0: 0, 1: 360 }, Infinity);
+let AnimateHalfcircleT = new Animate.property(Ahalfcircle, "angleTo", 1000, { 0: 180, 1: 540 }, Infinity);
+let AnimateHalfcircleIF = new Animate.property(AhalfcircleI, "angleFrom", 1000, { 0: 180, 1: 540 }, Infinity);
+let AnimateHalfcircleIT = new Animate.property(AhalfcircleI, "angleTo", 1000, { 0: 360, 1: 720 }, Infinity);
+
+let AnimateHalfcircleIX = new Animate.property(AhalfcircleI, "x", 2000, { 0: Game.width / 4, 0.5: Game.width * 3 / 4, 1: Game.width / 4 }, Infinity);
 
 Game.start();
