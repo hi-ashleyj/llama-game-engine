95a6bf7a894179109ea9cacfe1fc454b35abad11
Ashley J <48008893+hi-ashleyj@users.noreply.github.com>
2022-07-26 22:27:31 +0000

    motion stores

--- src/lib/components/motions.ts
+++ src/lib/components/motions.ts
@@ -1,14 +1,54 @@
+import { onDestroy } from "svelte";
+import { writable } from "svelte/store";
+import type { Writable } from "svelte/store";
+
 export class Timing {
 
-    targets: Set<string>;
-
     constructor() {
+        this.targets = new Set();
+    }
+    
+    targets: Set<{ current: number, duration: number, repeats: number, store: Writable<number | null> }>;
+    
+    /**
+     * Used to create a store whose value changes based on the total delta time.
+     * Duration is in MS
+     * Use 0 for repeats for infinite
+     */
+    create({ duration, repeats }: { duration: number, repeats: number }) {
+        let store = writable(0)
+        let out = {
+            current: 0,
+            duration,
+            repeats,
+            store
+        }
 
+        onDestroy(() => {
+            if (this.targets.has(out)) this.targets.delete(out);
+        })
+
+        this.targets.add(out);
+        return store;
     }
 
-    create({ duration, transform }: { duration: number, transform: (x: number) => number}) {
-        let timeSum = 0;
+    update(delta: number) {
+        this.targets.forEach((t, ignored, set) => {
+            if (t.current < 0) {
+                t.store.set(null);
+                set.delete(t);
+                return;
+            }
+            
+            t.current += delta;
+            if (t.repeats > 0 && t.current > t.duration * t.repeats) {
+                t.current = -1;
+                t.store.set(1);
+                return;
+            }
 
+            t.store.set((t.current / t.duration) % 1);
+        })
     }
 
 }
