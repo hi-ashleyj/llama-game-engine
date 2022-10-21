c131d8f50b2c7d35bd51b939557a3a553868e907
Ashley J <48008893+hi-ashleyj@users.noreply.github.com>
2022-10-21 02:37:22 +0000

    idk something why is vscode not doing the things

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,3 +3,7 @@ export { default as Layer } from "./components/Layer.svelte";
 export { default as GameObject } from "./components/GameObject.svelte";
 
 export * as drawables from "./components/drawables/index";
+export { setupDrawable } from "./setup";
+export { resolve as resolveImage } from "./components/resources/images";
+
+export { Timing } from "./components/motions";
