256d6e9a9ffdb0f23b981eb42ea9d24d23dcfbf4
Ashley J <48008893+hi-ashleyj@users.noreply.github.com>
2022-11-06 22:04:19 +0000

    eh multiline text maybe who cares?

---
+++ src/lib/components/drawables/MultiLineText.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+    import { setupDrawable } from "$lib/setup";
+    import type { DrawableFunction } from "$lib/types/contexts";
+    import { onMount } from "svelte";
+
+    export let text: string;
+    export let size: number;
+    export let spacing: number = 1.4;
+    export let font: string;
+    export let style: string | undefined = undefined;
+    export let fill: string | undefined = undefined;
+    export let stroke: string | undefined = undefined;
+    export let alignH: "left" | "center" | "right" = "left";
+    export let alignV: "top" | "middle" | "bottom" | "alphabetic" = "top";
+
+    $: computedFont = (style ? style + " " : "") + size + "px " + font;
+    $: splits = text?.split("\n") || [];
+
+    $: offset =
+        alignV === "bottom"
+            ? ((splits?.length || 1) - 1) * size * spacing
+            : alignV === "middle"
+            ? ((splits?.length || 1) - 1) * size * spacing * 0.5
+            : 0;
+
+    const draw: DrawableFunction = function (
+        { ctx },
+        { x, y }: { x: number; y: number; w: number; h: number }
+    ) {
+        if (!splits) return;
+
+        for (let i = 0; i < splits.length; i++) {
+            const line = splits[i];
+
+            ctx.beginPath();
+            ctx.textAlign = alignH;
+            ctx.textBaseline = alignV;
+            ctx.font = computedFont;
+
+            if (fill) {
+                ctx.fillStyle = fill;
+                ctx.fillText(line, x, y + size * spacing * i - offset);
+            }
+            if (stroke) {
+                ctx.strokeStyle = stroke;
+                ctx.strokeText(line, x, y + size * spacing * i - offset);
+            }
+        }
+    };
+
+    let register = setupDrawable({});
+
+    onMount(() => {
+        return register({ draw });
+    });
+</script>
--- src/lib/components/drawables/index.ts
+++ src/lib/components/drawables/index.ts
@@ -3,3 +3,4 @@ export { default as Circle } from "./Circle.svelte";
 export { default as Arc } from "./Arc.svelte";
 export { default as DisplayImage } from "./DisplayImage.svelte";
 export { default as Text } from "./Text.svelte";
+export { default as MultiLineText } from "./MultiLineText.svelte";
