5d6041364776ea2b2118c94e14406737c33e8e78
hi-ashleyj <ajor023@gmail.com>
2026-09-20 15:53:11 +1200

    in theory this makes audio usable

--- src/lib/audio/context.ts
+++ src/lib/audio/context.ts
@@ -23,10 +23,32 @@ export const getAudioContext = (): () => AudioContext => {
     return getGame().audio;
 }
 
-export const getConnector = (connect?: (node: AudioNode) => DisconnectFunction) => {
+export const getConnector = (connect?: (node: AudioNode) => DisconnectFunction): AudioSvelteContext ["connect"] => {
     const ctx = getContext<AudioSvelteContext>(CONNECTOR);
+    let waiting = true;
+    let delayed = new Map<Symbol, AudioNode | DisconnectFunction>();
     if (connect) {
-        setContext<AudioSvelteContext>(CONNECTOR, { connect });
+        setContext<AudioSvelteContext>(CONNECTOR, {
+            connect: (node: AudioNode) => {
+                if (!waiting) { return connect(node); }
+
+                const symbol = Symbol();
+                delayed.set(symbol, node);
+                return () => {
+                    const d = delayed.get(symbol);
+                    if (d && typeof d === "function") d();
+                }
+            }
+        });
     }
-    return ctx.connect;
+    // this function is me connecting
+    return (self) => {
+        waiting = false;
+        for (let k of delayed.keys()) {
+            const node = delayed.get(k)!;
+            if (typeof node === "function") continue;
+            if (connect) delayed.set(k, connect(node));
+        }
+        return ctx.connect(self);
+    };
 }
